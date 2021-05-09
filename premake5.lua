1e03eafef40bb53eb6d61560e2b665d755b33403
WhoseTheNerd <tutorialvideohd@gmail.com>
2021-05-09 20:32:19 +0300

    Little change

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
     kind "SharedLib"
     language "C"
-    staticruntime "on"
+    staticruntime "off"
 
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
